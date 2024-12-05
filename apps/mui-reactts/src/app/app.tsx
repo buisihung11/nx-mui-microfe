00ee771ad01842fc42113d3ee1ff8280eb804353
HungHcc <buisihung321@gmail.com>
2024-12-05 14:39:28 +0700

    feat: implement mui-reactts application with module federation support

---
+++ apps/mui-reactts/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="mui_reactts" />
+    </StyledApp>
+  );
+}
+
+export default App;
--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -2,16 +2,22 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 import { Link, Route, Routes } from 'react-router-dom';
 
+const MuiReactts = React.lazy(() => import('mui_reactts/Module'));
+
 export function App() {
   return (
-    <React.Suspense fallback={null}>
+    <React.Suspense fallback={<p>Loadding...</p>}>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/mui-reactts">MuiReactts</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="apps/shell" />} />
+        <Route path="/mui-reactts" element={<MuiReactts />} />
       </Routes>
     </React.Suspense>
   );
